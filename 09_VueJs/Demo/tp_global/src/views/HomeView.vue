ad48dff1f2b814a6a1be9fdc263aafcc20d5bb4d
Cindy Telmon <telcindy59@gmail.com>
2023-12-22 10:23:21 +0100

    ajout image et paragraphe dans la home

--- 09_VueJs/Demo/tp_global/src/views/HomeView.vue
+++ 09_VueJs/Demo/tp_global/src/views/HomeView.vue
@@ -2,6 +2,7 @@
 import { useAuthStore } from "../stores/auth";
 
 const authStore = useAuthStore();
+
 </script>
 
 <template>
@@ -30,10 +31,20 @@ const authStore = useAuthStore();
     <div class="container" v-if="authStore.isLoggedIn">
       <h4>Rick et Morty</h4>
       <div id="video-container">
-        <video autoplay loop id="video-background">
+        <video autoplay id="video-background">
           <source src="../assets/video.mp4" type="video/mp4" class="vid" />
         </video>
         <div id="video-overlay"></div>
+      </div>
+      <div class="description">
+        <p class="para-descri">
+        "Rick and Morty" est un dessin animé d'animation pour adultes qui mélange habilement la science-fiction, l'humour absurde, et des éléments de satire sociale. La série, créée par Justin Roiland et Dan Harmon, suit les aventures de Rick Sanchez, un scientifique génial mais égocentrique, et de son petit-fils Morty Smith, un adolescent ordinaire mais souvent malchanceux.</p>
+      </div>
+      <div class="morty">
+        <img src="../assets/mortySmith.png" alt="morty smith" class="img-morty">
+      </div>
+      <div class="rick">
+        <img src="../assets/rickSanchez.png" alt="rick sanchez" class="img-rick">
       </div>
     </div>
   </main>
@@ -53,13 +64,13 @@ h1 {
   background: linear-gradient(to right, #00ccff, #00ff99);
   -webkit-background-clip: text;
   color: transparent;
+  
 }
 .home-title,
 h2,
 h3 {
   color: white;
 }
-
 .gif {
   display: flex;
   justify-content: center;
@@ -93,9 +104,10 @@ h4 {
   -webkit-background-clip: text;
   color: transparent;
   text-align: center;
-  font-size: 50px;
+  font-size: 60px;
   margin: 0;
   padding-top: 20px;
+  text-shadow: 2px 2px 20px #ffff00;
 }
 .container {
   background-image: url("../assets/background-1.png");
@@ -147,5 +159,33 @@ h4 {
   width: 100%;
   height: 100%;
   object-fit: cover;
+}
+.description {
+  text-align: justify;
+  margin-top: 40px;
+  position: absolute;
+  top: 70%;
+  left: 50%;
+  transform: translate(-50%, -50%);
+  z-index: 1;
+  
+  
+}
+.para-descri {
+  color: rgb(233, 236, 236);
+  text-shadow: 1px 1px 2px red, 0 0 1em blue, 0 0 0.2em blue;
+  font-size: 17px;
+  font-weight: bold;
+}
+.img-morty {
+  width: 300px;
+  position: absolute;
+  left: 6%;
+}
+
+.img-rick {
+  width: 400px;
+  position: absolute;
+  left: 70%;
 }
 </style>
