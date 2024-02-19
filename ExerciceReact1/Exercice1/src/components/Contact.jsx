9f37b202031905cde7d2b2e671383033ce651c61
Cindy Telmon <telcindy59@gmail.com>
2024-02-19 10:54:03 +0100

    fix

--- ExerciceReact1/Exercice1/src/components/Contact.jsx
+++ ExerciceReact1/Exercice1/src/components/Contact.jsx
@@ -3,22 +3,22 @@ import React from 'react';
 const Contact = () => {
     return ( 
         <tbody>
-        <tr>
-            <td>Doe</td>
-            <td>John</td>
-            <td>0125469874</td>
-        </tr>
-        <tr>
-            <td>Smith</td>
-            <td>Jane</td>
-            <td>1245257841</td>
-        </tr>
-        <tr>
-            <td>Scott</td>
-            <td>David</td>
-            <td>124878655</td>
-        </tr>
-    </tbody>
+            <tr>
+                <td>Doe</td>
+                <td>John</td>
+                <td>0125469874</td>
+            </tr>
+            <tr>
+                <td>Smith</td>
+                <td>Jane</td>
+                <td>1245257841</td>
+            </tr>
+            <tr>
+                <td>Scott</td>
+                <td>David</td>
+                <td>124878655</td>
+            </tr>
+        </tbody>
      );
 }
  
